3b83780dc11dc6b468413219525f4ed63d47fd08
Tobias Meyer <52013820+Hope-IT-Works@users.noreply.github.com>
2022-05-16 14:34:16 +0200

    Create Install-PowerShell.ps1
    simple script for download and installing powershell

---
+++ src/Automation/Install-PowerShell.ps1
@@ -0,0 +1,38 @@
+function Check-IsElevated {
+    $a = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+    $b = New-Object System.Security.Principal.WindowsPrincipal($a)
+    if ($b.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)) {
+        $c = $true
+    } else {
+        $c = $false
+    }
+    return $c
+}
+
+if(-not(Check-IsElevated)){
+    return "Error: The script was not invoked as administrator"
+}
+
+if([System.Environment]::Is64BitOperatingSystem){
+    $PS_DL_ARCH = "x64"
+} else {
+    $PS_DL_ARCH = "x86"
+}
+
+$PS_DL_VERSION = "7.2.3"
+$PS_DL_URL     = "https://github.com/PowerShell/PowerShell/releases/download/v"+$PS_DL_VERSION+"/PowerShell-"+$PS_DL_VERSION+"-win-"+$PS_DL_ARCH+".msi"
+$PS_DL_NAME    = $PS_DL_URL.Split("/")
+$PS_DL_COUNT   = $PS_DL_NAME.Count-1
+$PS_DL_NAME    = $PS_DL_NAME[$PS_DL_COUNT]
+$PS_DL_PATH    = [System.Environment]::GetEnvironmentVariable("TEMP")+"\"+$PS_DL_NAME
+
+try {
+    Write-Host -ForegroundColor Yellow ("Downloading "+$PS_DL_NAME)
+    Invoke-WebRequest -UseBasicParsing -Uri $PS_DL_URL -OutFile $PS_DL_PATH
+    Write-Host -ForegroundColor Yellow ("Downloaded "+$PS_DL_NAME)
+    Write-Host -ForegroundColor Yellow ("Installing "+$PS_DL_NAME+" with msiexec")
+    msiexec /i $PS_DL_PATH /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=0 ADD_FILE_CONTEXT_MENU_RUNPOWERSHELL=0 ENABLE_PSREMOTING=1 REGISTER_MANIFEST=1 USE_MU=1 ENABLE_MU=1
+    Write-Host -ForegroundColor Yellow ("Installed "+$PS_DL_NAME)
+} catch {
+    Write-Host -ForegroundColor Red "Error: Download or Installation failed."
+}
